02e9787d4be326b2447c0e6e47f222437891fe0b
Joey Hines <joey@ahines.net>
2020-02-15 17:26:18 -0600

    Outputting JPEG images from web cam works
    + Now to actually do motion detection

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
-#include <stdio.h>
+/*
+ * Motion Detector
+ *
+ * Joey Hines
+ *
+ * Video capture code based off an example from:
+ * https://www.linuxtv.org/downloads/v4l-dvb-apis-new/uapi/v4l/capture.c.html
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,14 +15,20 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <errno.h>
+#include <malloc.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/time.h>
+#include <time.h>
 #include <sys/mman.h>
 #include <sys/ioctl.h>
-
+#include <asm/types.h>
 #include <linux/videodev2.h>
+#include <jpeglib.h>
 #include <libv4l2.h>
+#include <signal.h>
+#include <stdint.h>
+#include <inttypes.h>
 
 #define CLEAR(x) memset(&(x), 0, sizeof(x))
 
@@ -30,6 +44,17 @@ struct webcam_info {
     int num_of_buffers;
     int frame_count;
 };
+
+static int xioctl(int fd, int request, void* argp)
+{
+    int r;
+
+    do r = ioctl(fd, request, argp);
+    while (-1 == r && EINTR == errno);
+
+    return r;
+}
+
 
 static void open_device(struct webcam_info *cam_info) {
     struct stat st;
@@ -64,7 +89,7 @@ static void init_mmap(struct webcam_info * cam_info)
     req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
     req.memory = V4L2_MEMORY_MMAP;
 
-    if (-1 == ioctl(cam_info->fd, VIDIOC_REQBUFS, &req)) {
+    if (-1 == xioctl(cam_info->fd, VIDIOC_REQBUFS, &req)) {
         if (EINVAL == errno) {
             fprintf(stderr, "%s does not support memory mapping\n", cam_info->dev_name);
             exit(EXIT_FAILURE);
@@ -118,7 +143,7 @@ static void init_device(struct webcam_info *cam_info) {
     struct v4l2_format fmt;
     unsigned int min;
 
-    if (-1 == ioctl(cam_info->fd, VIDIOC_QUERYCAP, &cap)) {
+    if (-1 == xioctl(cam_info->fd, VIDIOC_QUERYCAP, &cap)) {
         if (EINVAL == errno) {
             fprintf(stderr, "%s is no V4L2 device\n", cam_info->dev_name);
             exit(EXIT_FAILURE);
@@ -144,7 +169,7 @@ static void init_device(struct webcam_info *cam_info) {
 
     cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
 
-    if (0 == ioctl(cam_info->fd, VIDIOC_CROPCAP, &cropcap)) {
+    if (0 == xioctl(cam_info->fd, VIDIOC_CROPCAP, &cropcap)) {
         crop.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
         crop.c = cropcap.defrect; /* reset to default */
 
@@ -166,10 +191,10 @@ static void init_device(struct webcam_info *cam_info) {
     CLEAR(fmt);
 
     fmt.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
-    fmt.fmt.pix.width       = 640;
-    fmt.fmt.pix.height      = 480;
+    fmt.fmt.pix.width       = 320;
+    fmt.fmt.pix.height      = 240;
     fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
-    fmt.fmt.pix.field       = V4L2_FIELD_INTERLACED;
+    fmt.fmt.pix.field       = V4L2_FIELD_NONE;
 
     if (-1 == ioctl(cam_info->fd, VIDIOC_S_FMT, &fmt)) {
         exit(-1);
@@ -238,14 +263,6 @@ static int read_frame(struct webcam_info *cam_info) {
 
     assert(buf.index < cam_info->num_of_buffers);
 
-    FILE * output = fopen("out.bin", "w");
-    if (output != NULL) {
-        fwrite(cam_info->buffers[buf.index].start,buf.bytesused , 1, output);
-        fclose(output);
-    }
-    else {
-        printf("Error opening output file %s\n", strerror(errno));
-    }
 
     fflush(stderr);
     fprintf(stderr, ".");
@@ -296,11 +313,95 @@ static void mainloop(struct webcam_info *cam_info) {
     }
 }
 
+void YUV420toYUV444(int width, int height, unsigned char* src, unsigned char* dst) {
+    int line, column;
+    unsigned char *py, *pu, *pv;
+    unsigned char *tmp = dst;
+
+    // In this format each four bytes is two pixels. Each four bytes is two Y's, a Cb and a Cr.
+    // Each Y goes to one of the pixels, and the Cb and Cr belong to both pixels.
+    unsigned char *base_py = src;
+    //unsigned char *base_pu = src+(height*width);
+    //unsigned char *base_pv = src+(height*width)+(height*width)/4;
+
+    for (line = 0; line < height; ++line) {
+        for (column = 0; column < width*2; column++) {
+            py = base_py+(line*width)+column*2;
+            //pu = base_pu+(line/2*width/2)+column/2;
+            //pv = base_pv+(line/2*width/2)+column/2;
+
+            *tmp++ = *py;
+            //*tmp++ = *pu;
+            //*tmp++ = *pv;
+        }
+    }
+}
+
+static void jpegWrite(unsigned char* src, char* jpegFilename)
+{
+    struct jpeg_compress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    unsigned char* tmp_row_buf = malloc(320*3*sizeof(char));
+
+    JSAMPROW row_pointer[1];
+    FILE *outfile = fopen( jpegFilename, "wb" );
+
+    // try to open file for saving
+    if (!outfile) {
+        exit(EXIT_FAILURE);
+    }
+
+    // create jpeg data
+    cinfo.err = jpeg_std_error( &jerr );
+    jpeg_create_compress(&cinfo);
+    jpeg_stdio_dest(&cinfo, outfile);
+
+    // set image parameters
+    cinfo.image_width = 320;
+    cinfo.image_height = 240;
+    cinfo.input_components = 3;
+    cinfo.in_color_space = JCS_YCbCr;
+
+    // set jpeg compression parameters to default
+    jpeg_set_defaults(&cinfo);
+    // and then adjust quality setting
+    jpeg_set_quality(&cinfo, 70, TRUE);
+
+    // start compress
+    jpeg_start_compress(&cinfo, TRUE);
+
+    // feed data
+    row_pointer[0] = &tmp_row_buf[0];
+    while (cinfo.next_scanline < cinfo.image_height) {
+        unsigned i, j;
+        unsigned offset = cinfo.next_scanline * cinfo.image_width * 2; //offset to the correct row
+        for (i = 0, j = 0; i < cinfo.image_width * 2; i += 4, j += 6) { //input strides by 4 bytes, output strides by 6 (2 pixels)
+            tmp_row_buf[j + 0] = src[offset + i + 0]; // Y (unique to this pixel)
+            tmp_row_buf[j + 1] = src[offset + i + 1]; // U (shared between pixels)
+            tmp_row_buf[j + 2] = src[offset + i + 3]; // V (shared between pixels)
+            tmp_row_buf[j + 3] = src[offset + i + 2]; // Y (unique to this pixel)
+            tmp_row_buf[j + 4] = src[offset + i + 1]; // U (shared between pixels)
+            tmp_row_buf[j + 5] = src[offset + i + 3]; // V (shared between pixels)
+        }
+        jpeg_write_scanlines(&cinfo, row_pointer, 1);
+    }
+
+    // finish compression
+    jpeg_finish_compress(&cinfo);
+
+    // destroy jpeg data
+    jpeg_destroy_compress(&cinfo);
+
+    // close output file
+    fclose(outfile);
+}
+
 int main() {
     struct webcam_info cam_info;
     cam_info.fd = -1;
     cam_info.dev_name = "/dev/video0";
-    cam_info.frame_count = 2;
+    cam_info.frame_count = 10;
 
     open_device(&cam_info);
 
@@ -313,11 +414,22 @@ int main() {
     mainloop(&cam_info);
 
     stop_capturing(&cam_info);
+
+
+    for (int i = 0; i < cam_info.num_of_buffers; ++i) {
+        char file_name[100];
+
+        snprintf(file_name, 100, "out%d.jpg", i);
+
+        jpegWrite(cam_info.buffers[i].start, file_name);
+    }
+
     for (int i = 0; i < cam_info.num_of_buffers; ++i) {
         if (-1 == munmap(cam_info.buffers[i].start, cam_info.buffers[i].length)) {
             exit(EXIT_FAILURE);
         }
     }
+
 
     close(cam_info.fd);
     return 0;
